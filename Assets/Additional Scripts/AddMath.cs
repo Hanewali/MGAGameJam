b302444f9a6a5cf0bc4d7d6a9637082122e62b99
Jakub Rumpel <jakub@rumpel.pl>
2018-11-03 20:22:08 +0100

    Shopkeeper is seen by click

---
+++ Assets/Additional Scripts/AddMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Additional_Scripts
+{
+    class AddMath
+    {
+        public static float Pitagorem(float a, float b)
+        {
+            return (float)Math.Sqrt((a * a) + (b * b));
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Additional_Scripts;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.SceneManagement;
@@ -8,13 +9,16 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D Player;
     public Animator PlayerAnimator;
+    public Rigidbody2D ShopKeeper;
     private Vector2 _speed = new Vector2(10f, 10f);
     private float _horizontalInput;
     private float _verticalInput;
+    private float _distanceToShopKeeper;
 
     // Use this for initialization
     void Start ()
-	{
+    {
+        ShopKeeper = GetComponent<Rigidbody2D>();
 	    Player = GetComponent<Rigidbody2D>();
 	    PlayerAnimator = GetComponent<Animator>();
 	}
@@ -22,46 +26,19 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        //Debug.Log(SceneManager.GetActiveScene().name);
-	    //Debug.Log(PlayerAnimator.Get);
-	    if (OtherValues.State1)
-	    {
-            PlayerAnimator.SetBool("State1", true);
-	        PlayerAnimator.SetBool("State2", false);
-	        PlayerAnimator.SetBool("State3", false);
-	        PlayerAnimator.SetBool("State4", false);
-        }
-
-	    if (OtherValues.State2)
-	    {
-	        PlayerAnimator.SetBool("State1", false);
-	        PlayerAnimator.SetBool("State2", true);
-	        PlayerAnimator.SetBool("State3", false);
-	        PlayerAnimator.SetBool("State4", false);
-        }
-
-	    if (SceneManager.GetActiveScene().name == "AlbertHouse")
-	    {
-	        PlayerAnimator.SetBool("State1", false);
-	        PlayerAnimator.SetBool("State2", false);
-	        PlayerAnimator.SetBool("State3", true);
-	        PlayerAnimator.SetBool("State4", false);
-        }
-
-	    if (OtherValues.State4)
-	    {
-	        PlayerAnimator.SetBool("State1", false);
-	        PlayerAnimator.SetBool("State2", false);
-	        PlayerAnimator.SetBool("State3", false);
-	        PlayerAnimator.SetBool("State4", true);
-        }
 	     _horizontalInput = Input.GetAxis("Horizontal");
 	     _verticalInput = Input.GetAxis("Vertical");
-    }
+	    if (_distanceToShopKeeper < 20 && Input.GetKeyDown(KeyCode.E))
+	    {
+            //TODO: Rozpoczęcie dialogu z ShopKeeperem
+            Debug.Log("Dialogue Started!");
+	    }
+	}
 
     void FixedUpdate()
     {
         Player.velocity = new Vector2(_speed.x * _horizontalInput, _speed.y * _verticalInput);
+        _distanceToShopKeeper = AddMath.Pitagorem(Mathf.Abs(Player.transform.position.x - ShopKeeper.transform.position.x), (Player.transform.position.y - ShopKeeper.transform.position.y));
     }
 
     void OnTriggerEnter2D(Collider2D trig)
@@ -69,34 +46,18 @@ public class PlayerController : MonoBehaviour
         if (trig.gameObject.name == "FrankHouse")
         {
             SceneManager.LoadScene("FrankHouse");
-            OtherValues.State1 = false;
-            OtherValues.State2 = true;
-            OtherValues.State3 = false;
-            OtherValues.State4 = false;
         }
         else if (trig.gameObject.name == "AlbertHouse")
         {
             SceneManager.LoadScene("AlbertHouse");
-            OtherValues.State1 = false;
-            OtherValues.State2 = false;
-            OtherValues.State3 = true;
-            OtherValues.State4 = false;
         }
         else if (trig.gameObject.name == "SinatraHouse")
         {
             SceneManager.LoadScene("SinatraHouse");
-            OtherValues.State1 = false;
-            OtherValues.State2 = false;
-            OtherValues.State3 = false;
-            OtherValues.State4 = true;
         }
         if (trig.gameObject.name == "Out")
         {
             SceneManager.LoadScene("Main");
-            OtherValues.State1 = true;
-            OtherValues.State2 = false;
-            OtherValues.State3 = false;
-            OtherValues.State4 = false;
         }
     }
 }
