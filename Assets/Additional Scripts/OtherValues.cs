905fa098c89cd67a843bc329cc4965a20828c886
Jakub Rumpel <jakub@rumpel.pl>
2018-11-03 17:50:39 +0100

    Syncing

---
+++ Assets/Additional Scripts/OtherValues.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherValues : MonoBehaviour
+{
+
+    public static bool State1;
+    public static bool State2;
+    public static bool State3;
+    public static bool State4;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,6 +22,39 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+        //Debug.Log(SceneManager.GetActiveScene().name);
+	    //Debug.Log(PlayerAnimator.Get);
+	    if (OtherValues.State1)
+	    {
+            PlayerAnimator.SetBool("State1", true);
+	        PlayerAnimator.SetBool("State2", false);
+	        PlayerAnimator.SetBool("State3", false);
+	        PlayerAnimator.SetBool("State4", false);
+        }
+
+	    if (OtherValues.State2)
+	    {
+	        PlayerAnimator.SetBool("State1", false);
+	        PlayerAnimator.SetBool("State2", true);
+	        PlayerAnimator.SetBool("State3", false);
+	        PlayerAnimator.SetBool("State4", false);
+        }
+
+	    if (SceneManager.GetActiveScene().name == "AlbertHouse")
+	    {
+	        PlayerAnimator.SetBool("State1", false);
+	        PlayerAnimator.SetBool("State2", false);
+	        PlayerAnimator.SetBool("State3", true);
+	        PlayerAnimator.SetBool("State4", false);
+        }
+
+	    if (OtherValues.State4)
+	    {
+	        PlayerAnimator.SetBool("State1", false);
+	        PlayerAnimator.SetBool("State2", false);
+	        PlayerAnimator.SetBool("State3", false);
+	        PlayerAnimator.SetBool("State4", true);
+        }
 	     _horizontalInput = Input.GetAxis("Horizontal");
 	     _verticalInput = Input.GetAxis("Vertical");
     }
@@ -36,6 +69,34 @@ public class PlayerController : MonoBehaviour
         if (trig.gameObject.name == "FrankHouse")
         {
             SceneManager.LoadScene("FrankHouse");
+            OtherValues.State1 = false;
+            OtherValues.State2 = true;
+            OtherValues.State3 = false;
+            OtherValues.State4 = false;
+        }
+        else if (trig.gameObject.name == "AlbertHouse")
+        {
+            SceneManager.LoadScene("AlbertHouse");
+            OtherValues.State1 = false;
+            OtherValues.State2 = false;
+            OtherValues.State3 = true;
+            OtherValues.State4 = false;
+        }
+        else if (trig.gameObject.name == "SinatraHouse")
+        {
+            SceneManager.LoadScene("SinatraHouse");
+            OtherValues.State1 = false;
+            OtherValues.State2 = false;
+            OtherValues.State3 = false;
+            OtherValues.State4 = true;
+        }
+        if (trig.gameObject.name == "Out")
+        {
+            SceneManager.LoadScene("Main");
+            OtherValues.State1 = true;
+            OtherValues.State2 = false;
+            OtherValues.State3 = false;
+            OtherValues.State4 = false;
         }
     }
 }
